419922a4df50f57a8c21999c2be68baa1ae7bac1
bbensch09 <bbensch@gmail.com>
2015-10-27 01:48:36 -0700

    update basic formatting functionality for core sections oct26

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ActiveRecord::Base
   has_many :comments
-  validates :title, presence: true,
-                    length: { minimum: 5 }
+  validates  :title, :intro, :need_to_know, :recap, :hot_and_cold, :opp_lineup, :sharks_lineup, :further_reading, :memorable_moments, :share,
+            presence: true
+            # length: { minimum: 5 }
 end
+
+
