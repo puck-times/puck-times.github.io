05db30120ffe6d79287c19bd25067c1c305b03e3
bbensch09 <bbensch@gmail.com>
2015-10-31 14:32:01 -0700

    paperclip.rb test comment out everything

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,10 @@
 # config/initializers/paperclip.rb
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+# Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 
-Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+# Paperclip::Attachment.default_options[:url] = 'https://console.aws.amazon.com/s3/home?region=us-west-2&bucket=puck-times&prefix='
+# Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+
+#example actual URL
+# https://s3-us-west-1.amazonaws.com/puck-times/articles/photo2s/000/000/005/original/rinne-nashville-oct31.jpg?
+
+#Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
